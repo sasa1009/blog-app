5e56dc02d092d98804140c369615ef94c4db722f
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2021-01-24 00:30:25 +0900

    Finish section 3

---
+++ src/NewPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <PostWriter :post="post" @save="save" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PostWriter from './PostWriter.vue'
+import { Post } from './Types'
+import moment=require('moment')
+
+export default defineComponent({
+  name: 'NewPost',
+
+  components: {
+    PostWriter
+  },
+
+  setup() {
+    const post: Post = {
+      id: -1,
+      title: 'New Post',
+      markdown: '## New Post\nEnter your post here...',
+      html: '',
+      created: moment(),
+      authorId: 0
+    }
+
+    const save = (post: Post) => {
+      console.log('Hello', post)
+    }
+
+    return {
+      post,
+      save
+    }
+  }
+})
+</script>
